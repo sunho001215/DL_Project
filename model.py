2de0fde42a44dbe7558329ee90b23a15108fc77c
sunho001215 <sunho001215@gmail.com>
2020-12-04 11:55:48 +0900

    add data preprocessing code

--- model.py
+++ model.py
@@ -34,3 +34,34 @@ class Generator(nn.Module):
         x = F.tanh(self.deconv5(x))
 
         return x
+
+
+class Discriminator(nn.Module):
+    def __init__(self, d = 128):
+        super(Discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, 2*d, 4, 2, 1)
+        self.bn2 = nn.BatchNorm2d(2*d)
+        self.conv3 = nn.Conv2d(2*d, 4*d, 4, 2, 1)
+        self.bn3 = nn.BatchNorm2d(4*d)
+        self.conv4 = nn.Conv2d(4*d, 8*d, 4, 2, 1)
+        self.bn4 = nn.BatchNorm2d(8*d)
+        self.conv5 = nn.Conv2d(8*d, 1, 4, 1, 0)
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+    
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2)
+        x = F.sigmoid(self.conv5(x))
+
+        return x
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
